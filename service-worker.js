318110d2fba9331922dceef98b592391e756d22a
YitzchakG <35669140+YitzchakG@users.noreply.github.com>
2025-05-13 12:55:28 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,11 @@
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('quiz-cache').then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
